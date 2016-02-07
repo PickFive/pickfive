47d56538cfd9871928bb8a0230a9737d8af151a9
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-02-07 17:15:05 -0500

    got some basic UI going

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Raive' });
 });
 
+router.get('/signup', function (req, res, next) {
+  res.render('users/signup', {message: req.flash() });
+});
+
+router.get('/signin', function (req, res, next) {
+  res.render('users/signin', {message: req.flash() });
+});
+
 module.exports = router;
