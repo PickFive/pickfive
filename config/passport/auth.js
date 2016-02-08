cbc2911b22763a2b9e09a6a3380bf00c68c243d9
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-02-08 11:39:39 -0500

    added profile fields to facebook configuration

--- config/passport/auth.js
+++ config/passport/auth.js
@@ -2,6 +2,7 @@ module.exports = {
  'facebookAuth': {
    'clientID': process.env.FACEBOOK_APP_ID,
    'clientSecret': process.env.FACEBOOK_APP_SECRET,
-   'callbackURL': 'https://raive.herokuapp.com/auth/facebook/callback'
+   'callbackURL': 'https://raive.herokuapp.com/auth/facebook/callback',
+   'profileFields': ['id', 'email', 'name']
  } 
 }
