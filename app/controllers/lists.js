8977cbc12f5ba06253ede04dadaa9cc383e859e6
Steve Smith <stevewillsmith@gmail.com>
2016-02-10 14:01:04 -0500

    delete works now

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -82,7 +82,7 @@ module.exports = (model) => {
   const destroy = (req, res, next) => {
     model.findByIdAndRemove(req.params.id)
       .then(() => {
-        res.redirect('lists');
+        res.redirect('/');
       }).catch((err) => {
         next(err);
       })
